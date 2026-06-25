b747b4d5d38f14176aad8306e765748690421125
iamgonnarockit <adhikarisadikshya097@gmail.com>
2026-06-25 19:03:25 +0545

    Data Types

---
+++ basic_datatypes.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+ //Integers- Whole Number
+ int age= 20;
+ int year= 2005;
+ int months=12;
+ int day=06;
+
+ cout << age <<"\n";
+ cout << year << "\n";
+ cout << months << "\n";
+ cout << day <<"\n  \n \n";
+ 
+ //double- number including decimals, also known as float in C.
+ double price= 250.30;
+ double height=5.6;
+
+ cout << price << "\n";
+ cout << height << "\n \n \n";
+
+ // char- a letter or a single character
+ char grade= 'A';
+ char  relationship='Y';
+
+ cout << grade <<"\n";
+ cout << relationship<<"\n \n \n";
+ 
+ // string- a combined group of letters or that represents a seq. of text.
+ string name="Rosy";
+ string gender="Female";
+
+ cout<< "My name is" << name  <<"and I am a" ;
+ cout << gender << "\n";
+
+ //boolean (true or false)
+  bool student=false;
+  bool talent=true;
+
+  cout<< student <<"\n"; 
+  cout<< talent;
+return 0;
+}
